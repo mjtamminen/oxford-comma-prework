485884c090c8916e5029d3e7defdce4118b33f4a
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-27 05:50:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-  array.join(", ")
+  if array.length == 1
+    return array
+  else
+    last_one = ", and #{array.last}"
+    array.pop
+    return "#{array.join(", ")} && #{last_one}"
+  end
 end
+  
+  
