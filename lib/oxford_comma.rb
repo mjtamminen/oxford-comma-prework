8bf0e0b2e464f5532ebe55961e81767259573e31
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-27 05:52:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
     return "#{array.first}"
+  elsif array.length == 2
+    return "#{array.first} and #{array.last}"
   else
     last_one = ", and #{array.last}"
     array.pop
